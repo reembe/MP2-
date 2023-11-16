2ffd08e7ba7971b9a38ad0ee6863477aa182e4da
Reem Belafkih <130573894+reembe@users.noreply.github.com>
2023-11-15 21:56:19 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main
+  {
+    public static void main(String[] args)
+    {
+      int j = 0;
+      while(j != 3)
+        {
+          for(int i = 0; i > 7; i++)
+        {
+          System.out.print(i);
+        }
+          j++;
+        }
+    }
+  }
