aa2f9e85f6cb65e899222c86121ac5a71d55daed
Reem Belafkih <130573894+reembe@users.noreply.github.com>
2023-12-03 16:34:36 -0500

    Update Words.java

--- Words.java
+++ Words.java
@@ -3,6 +3,18 @@ public Words
     public String getWords()
       {
       // return mutable object here
+        private int length;
+  public Sentence(int l)
+  {
+    length = l;
+  }
+
+  public int getLength()
+  {
+    return length;
+  }
+}
+
       }
 
     public String toString()
