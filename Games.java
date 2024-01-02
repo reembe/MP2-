c78e4ab42d955dfabb5c955861028d7b14a918b5
Reem Belafkih <130573894+reembe@users.noreply.github.com>
2024-01-01 20:30:26 -0500

    Add files via upload

--- Games.java
+++ Games.java
@@ -28,10 +28,16 @@ public class Games implements ActionListener{
     {
         if (language.equals("french"))
         {
-
+            String[] qf = {"1. How many pronouns are there?", "2. What are the basic conjugates?", "3. What tense is for a continuous past event?", "4. How many francaphone countries are there?", "5. How do you conjugate an IR verb using the nous pronoun?", "6. How do you say Hello in french?", "7. How many tenses are there?", "8. Who is the best french actor (Alive)?", "9. When was the first appearence of french in records?", "10. "};
         }
         if (language.equals("spanish"))
         {
+            String[] qs = {"1. What is 3 in Spanish?", "2. Which of the following means \"cat\" in Spanish?", "3. What is the female pronoun?","4. How many Spanish speaking countries are there?", "5. Which of the following is the imperfect tense for the verb \"ser\" in the yo form?", "6. Which of the following greets means \"good morning\" in Spanish?", "7. How do you " };
+            String[] as = {};
+            String[] bs = {};
+            String[] cs = {};
+            String[] ds = {};
+
 
         }
 
@@ -39,7 +45,7 @@ public class Games implements ActionListener{
 
     public void Listening()
     {
-             if (language.equals("french"))
+        if (language.equals("french"))
         {
 
         }
@@ -52,7 +58,7 @@ public class Games implements ActionListener{
 
     public void fillInTheBlank()
     {
-             if (language.equals("french"))
+        if (language.equals("french"))
         {
 
         }
--- introductionMenu.java
+++ introductionMenu.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
-
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JButton;
